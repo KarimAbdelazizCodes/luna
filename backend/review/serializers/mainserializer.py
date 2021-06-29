57567a9f4ec1b2f276252424d2ac69f5f8b3a4f7
Karim Abdelaziz <karimabdelaziz@live.com>
2021-06-29 20:27:07 +0200

    serializers done

--- backend/review/serializers/mainserializer.py
+++ backend/review/serializers/mainserializer.py
@@ -1,8 +1,51 @@
 from rest_framework import serializers
+
+from restaurant.models import Restaurant
 from review.models import Review
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+# nested user serializer for reviews
+class UserReviewSerializer(serializers.ModelSerializer):
+
+    number_of_reviews = serializers.SerializerMethodField()
+
+    def get_number_of_reviews(self, obj):
+        return obj.reviews.count()
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'first_name',
+            'last_name',
+            'number_of_reviews',
+            'avatar'
+        ]
+
+
+class NestedRestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = [
+            'id',
+            'name',
+        ]
 
 
 class MainReviewSerializer(serializers.ModelSerializer):
+    author = UserReviewSerializer(read_only=True)
+    restaurant = NestedRestaurantSerializer(read_only=True)
+    number_of_likes = serializers.SerializerMethodField()
+    number_of_comments = serializers.SerializerMethodField()
+
+    def get_number_of_likes(self, obj):
+        return obj.liked_by.count()
+
+    def get_number_of_comments(self, obj):
+        return obj.comments.count()
 
     class Meta:
         model = Review
