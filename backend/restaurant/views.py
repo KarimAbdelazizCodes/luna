348353083d0fdb960f7ee23b7535bb17b8210df0
Jon Strickler <jon.strickler@bluewin.ch>
2021-06-29 14:47:01 +0200

    add restaurant views with category model

--- backend/restaurant/views.py
+++ backend/restaurant/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from restaurant.models import Restaurant, Category
+from restaurant.permissions import IsOwnerOrSuperuserOrReadOnly
+from restaurant.serializers import RestaurantsSerializer, CategoriesSerializer
 
-# Create your views here.
+
+class ListRestaurantsView(ListAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantsSerializer
+
+
+class CreateRestaurantView(CreateAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantsSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ListRestaurantsByCategoryView(ListAPIView):
+    def get_queryset(self):
+        return Restaurant.objects.filter(category=self.kwargs["category_id"])
+
+    serializer_class = RestaurantsSerializer
+
+
+class ListRestaurantsByOwnerView(ListAPIView):
+    def get_queryset(self):
+        return Restaurant.objects.filter(owner=self.kwargs["owner_id"]).order_by("-created")
+
+    serializer_class = RestaurantsSerializer
+
+
+class RetrieveUpdateDestroyRestaurantView(RetrieveUpdateDestroyAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantsSerializer
+    lookup_field = 'id'
+
+
+class ListCategoriesView(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoriesSerializer
+
+
+
+
+
