c71d1bf393f4a83f2b8a59bc5df42f2edafc4443
Jon Strickler <jon.strickler@bluewin.ch>
2021-07-01 18:36:55 +0200

    add styles to create new restaurant

--- frontend/src/pages/NewRestaurant/index.js
+++ frontend/src/pages/NewRestaurant/index.js
@@ -7,48 +7,7 @@ import styled from 'styled-components';
 import Axios from '../../api';
 import {useDispatch, useSelector} from "react-redux";
 import {fetchCategories} from "../../store/actions/get_categories";
-
-
-const NewRestaurantWrapper = styled(PageWrapper)`
-  
-    main {
-      width: 90%;
-    }
-  
-  
-`
-
-const InputWrapper = styled.div`
-    
-  
-    input {
-      
-    border: 1px solid gray;
-    width: 200px;
-    height: 30px;
-        ::placeholder {
-          color: black;
-        }
-    }
-  
-    div {
-    padding: 0;
-    width: 100%;
-    height: 100%;
-    
-        button {
-          margin: 0;
-          padding: 0;
-        }
-    }
-`
-
-const Row = styled.div`
-      display: flex;
-      justify-content: space-between;
-`
-
-
+import {Wrapper} from "./styled";
 
 const NewRestaurantPage = (props) => {
     const dispatch = useDispatch()
@@ -63,29 +22,22 @@ const NewRestaurantPage = (props) => {
     const submitForm = async (data) => {
         const url = 'restaurants/new/';
         const formData = new FormData();
-
         for (const [key, value] of Object.entries(data)) {
             formData.append(key, value)
         }
-
-        console.log(RestaurantImage)
         formData.append("avatar", RestaurantImage);
-
-
-
         const config = {
             headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
         };
         try {
             const response = await Axios.post(url, formData, config);
             if (response.status === 201) {
-                //props.history.push('/restaurant')
-                alert('success')
+                props.history.push('/user/restaurants/')
             } else {
                 console.log(response.status)
             }
         } catch (error) {
-            console.log('error:', error)
+            alert('Category and Price level must be selected')
         }
     }
 
@@ -100,93 +52,88 @@ const NewRestaurantPage = (props) => {
         setRestaurantImage(e.target.files[0]);
     }
 
-
-
-
-
     return (
-        <NewRestaurantWrapper>
+        <Wrapper>
             <Header/>
-            <div>
-                <div className='title_decorator'>
-                    <h1>create new restaurant</h1>
-                </div>
-                <form onSubmit={handleSubmit(submitForm)} id='newRestaurant'>
-                    <h5>Basic</h5>
-                    <Row>
-                        <InputWrapper>
-                            <input placeholder='Name' type='text' {...register('name', { required: 'This field is required' })} />
+            <h1>create new restaurant</h1>
+            <main>
+                <div className="container">
+                    <form onSubmit={handleSubmit(submitForm)} id='newRestaurant'>
+                        <div>
+                            <h5>Name</h5>
+                            <input type='text' {...register('name', { required: 'This field is required' })} />
                             {errors.name && <p>{errors.name.message}</p>}
-                        </InputWrapper>
-                        <InputWrapper>
-                            <select placeholder='Category' type='text' {...register('category', { required: 'This field is required' })} >
-                                <option>Select a category</option>
+                        </div>
+                        <div>
+                            <h5>Category</h5>
+                            <select type='text' {...register('category', { required: 'This field is required' })} >
+                                <option disabled selected hidden>Select a value...</option>
                                 {categories.map(category => <option key={category.id} value={category.id}>{category.category}</option>)}
                             </select>
                             {errors.category && <p>{errors.category.message}</p>}
-                        </InputWrapper>
-                        <InputWrapper>
-                            <input placeholder='Country' type='text' {...register('country', { required: 'This field is required' })} />
+                        </div>
+                        <div>
+                            <h5>Country</h5>
+                            <input type='text' {...register('country', { required: 'This field is required' })} />
                             {errors.country && <p>{errors.country.message}</p>}
-                        </InputWrapper>
-                    </Row>
-                    <h5>Address</h5>
-                    <Row>
-                        <InputWrapper>
-                            <input placeholder='Street' type='text' {...register('street', { required: 'This field is required' })} />
+                        </div>
+                        <div>
+                            <h5>Street</h5>
+                            <input type='text' {...register('street', { required: 'This field is required' })} />
                             {errors.street && <p>{errors.street.message}</p>}
-                        </InputWrapper>
-                        <InputWrapper>
-                            <input placeholder='City' type='text' {...register('city', { required: 'This field is required' })} />
+                        </div>
+                        <div>
+                            <h5>City</h5>
+                            <input type='text' {...register('city', { required: 'This field is required' })} />
                             {errors.city && <p>{errors.city.message}</p>}
-                        </InputWrapper>
-                        <InputWrapper>
-                            <input placeholder='Zip' type='number' {...register('zip', { required: 'This field is required' })} />
+                        </div>
+                        <div>
+                            <h5>Zip</h5>
+                            <input type='number' {...register('zip', { required: 'This field is required' })} />
                             {errors.zip && <p>{errors.zip.message}</p>}
-                        </InputWrapper>
-                    </Row>
-                    <h5>Contact</h5>
-                    <Row>
-                        <InputWrapper>
-                            <input placeholder='Website' type='text' {...register('website', { required: 'This field is required' })} />
+                        </div>
+                        <div>
+                            <h5>Website</h5>
+                            <input type='text' {...register('website', { required: 'This field is required' })} />
                             {errors.website && <p>{errors.website.message}</p>}
-                        </InputWrapper>
-                        <InputWrapper>
-                            <input placeholder='Phone' type='number' {...register('phone_number', { required: 'This field is required' })} />
-                            {errors.phone && <p>{errors.phone.message}</p>}
-                        </InputWrapper>
-                        <InputWrapper>
-                            <input placeholder='Email' type='email' {...register('email', { required: 'This field is required' })} />
+                        </div>
+                        <div>
+                            <h5>Phone</h5>
+                            <input type='number' {...register('phone_number', { required: 'This field is required' })} />
+                            {errors.phone_number && <p>{errors.phone_number.message}</p>}
+                        </div>
+                        <div>
+                            <h5>Email</h5>
+                            <input type='email' {...register('email', { required: 'This field is required' })} />
                             {errors.email && <p>{errors.email.message}</p>}
-                        </InputWrapper>
-                    </Row>
-                    <h5>Details</h5>
-                    <Row>
-                        <InputWrapper>
-                            <input placeholder='Opening hours' type='text' {...register('hours', { required: 'This field is required' })} />
+                        </div>
+                        <div>
+                            <h5>Opening hours</h5>
+                            <input type='text' {...register('hours', { required: 'This field is required' })} />
                             {errors.hours && <p>{errors.hours.message}</p>}
-                        </InputWrapper>
-                        <InputWrapper>
-                            <select placeholder='Price level' type='number' {...register('price_level', { required: 'This field is required' })}>
+                        </div>
+                        <div>
+                            <h5>Price level</h5>
+                            <select type='number' {...register('price_level', { required: 'This field is required' })}>
+                                <option disabled selected hidden>Select a value...</option>
                                 <option value={1}>$</option>
                                 <option value={2}>$$</option>
-                                <option value={3}>$$$</option>
-                            </select>
-                            {errors.price && <p>{errors.price.message}</p>}
-                        </InputWrapper>
-                        <InputWrapper>
-                            <div>
-                                <input type="file" style={{display: "none"}} ref={realFileInput} onChange={e => handlePostPic(e)} accept="image/png, image/jpeg" multiple/>
-                                <p>{RestaurantImage.name}</p>
-                                <button onClick={e => replaceFileInput(e)}>CHOOSE A FILE...</button>
-                            </div>
-                        </InputWrapper>
-                    </Row>
-                </form>
-                <button type='submit' form='newRestaurant'>submit</button>
-            </div>
+                            <option value={3}>$$$</option>
+                        </select>
+                        {errors.price && <p>{errors.price.message}</p>}
+                        </div>
+                        <div>
+                            <input type="file" style={{display: "none"}} ref={realFileInput} onChange={e => handlePostPic(e)} accept="image/png, image/jpeg" multiple/>
+                            <h5>Image</h5>
+                            <span>{RestaurantImage.name}</span>
+                            <button onClick={e => replaceFileInput(e)}>CHOOSE A IMAGE...</button>
+                        </div>
+                    </form>
+                </div>
+            </main>
+            <button type='submit' form='newRestaurant'>submit</button>
             <Footer/>
-        </NewRestaurantWrapper>
+        </Wrapper>
     )
 }
 
